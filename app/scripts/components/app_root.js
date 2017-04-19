f73877b673d370476b3ca07e470b7d67c04b221c
Devon Moubry <devon@moubry.com>
2017-04-19 18:45:11 -0500

    add display text from markdown to preview

--- app/scripts/components/app_root.js
+++ app/scripts/components/app_root.js
@@ -1,28 +1,40 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { Route, Link, NavLink } from 'react-router-dom'
+import { Route } from 'react-router-dom'
 import container from '../containers/all.js'
-
-
-const NavBar = () => {
-  return (
-    <nav>
-      <NavLink to="/">Home</NavLink>
-    </nav>
-  )
-}
 
 class AppRoot extends React.Component {
   constructor (props) {
     super(props)
+
+    this.updatePreview = this.updatePreview.bind(this);
+    this.submitMarkdownNotes = this.submitMarkdownNotes.bind(this);
+  }
+
+  updatePreview() {
+    const markdownInput = this.refs.markdown.value;
+    this.props.dispatch({ type: "UPDATE_PREVIEW", markdownPreview: markdownInput });
+    console.log('I can see dead code');
+  }
+
+  submitMarkdownNotes()  {
+    //this.props.dispatch({ type: "SUBMIT_NOTES" });
+    console.log('I submitted the Markdown Notes to backendless');
   }
 
   render () {
     return (
       <main>
-        <NavBar/>
-        <section>
-          <h1>Markdown App</h1>
+        <section className="markdown-container">
+          <h1 className="markdown-title">Markdown App</h1>
+          <textarea onChange={this.updatePreview} className="textarea" ref="markdown"/>
+          <div className="submit-button-container">
+          <button className="submit-button" type="submit" onClick={this.submitMarkdownNotes}>Submit</button>
+          </div>
+        </section>
+        <section className="preview-container">
+          <h1 className="preview-title">Preview</h1>
+          <textarea className="textarea" value={this.props.markdownPreview}/>
         </section>
       </main>
     );
--- app/scripts/reducers/app_reducer.js
+++ app/scripts/reducers/app_reducer.js
@@ -1,14 +1,15 @@
 export default function AppReducer(state, action) {
   if (state === undefined) {
-    return {};
+    return {
+      markdownPreview: ''
+    };
   }
 
   switch (action.type) {
-    case "TEST_REDUCER":
-      // DELETE OR CHANGE ME: I AM JUST AN EXAMPLE
-      return Object.assign({}, state, { newData: action.data });
-  }
+    case "UPDATE_PREVIEW":
+      return Object.assign({}, state, { markdownPreview: action.markdownPreview });
 
-  console.log("Unhandled State!");
-  return state;
+    default:
+      return state;
+  }
 }
