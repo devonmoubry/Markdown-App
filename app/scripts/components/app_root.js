1bfa65319c1bba7ee7e1a85ccaf09181dad0fced
Devon Moubry <devon@moubry.com>
2017-04-19 19:57:55 -0500

    clear textarea and preview after saved

--- app/scripts/components/app_root.js
+++ app/scripts/components/app_root.js
@@ -1,52 +1,64 @@
 import React from 'react'
-import { connect } from 'react-redux'
-import { Route } from 'react-router-dom'
+import {
+    connect
+} from 'react-redux'
+import {
+    Route
+} from 'react-router-dom'
 import container from '../containers/all.js'
 import marked from 'marked'
 
 class AppRoot extends React.Component {
-  constructor (props) {
-    super(props)
+    constructor(props) {
+        super(props)
 
-    this.updatePreview = this.updatePreview.bind(this);
-    this.submitMarkdownNotes = this.submitMarkdownNotes.bind(this);
-    this.createMarkup = this.createMarkup.bind(this);
-  }
+        this.updatePreview = this.updatePreview.bind(this);
+        this.submitMarkdownNotes = this.submitMarkdownNotes.bind(this);
+        this.createMarkup = this.createMarkup.bind(this);
+    }
 
-  updatePreview() {
-    const markdownInput = this.refs.markdown.value;
-    this.props.dispatch({ type: "UPDATE_PREVIEW", markdownPreview: markdownInput });
-    console.log('I can see dead code');
-  }
+    updatePreview() {
+        const markdownInput = this.refs.markdown.value;
+        this.props.dispatch({
+            type: "UPDATE_PREVIEW",
+            markdownPreview: markdownInput
+        });
+        console.log('I can see dead code');
+    }
 
-  submitMarkdownNotes()  {
-    const markdownInput = this.refs.markdown.value;
-    console.log(markdownInput);
-    this.props.dispatch({ type: "SUBMIT_NOTES", markdownNotes: markdownInput });
-    console.log('I submitted the Markdown Notes to backendless');
-  }
+    submitMarkdownNotes() {
+        const markdownInput = this.refs.markdown.value;
+        console.log(markdownInput);
+        this.props.dispatch({
+            type: "SUBMIT_NOTES",
+            markdownNotes: markdownInput
+        });
+        console.log('I submitted the Markdown Notes to backendless');
+    }
 
-  createMarkup() {
-    return {__html: marked(this.props.markdownPreview)}
-  }
+    createMarkup() {
+        return {
+            __html: marked(this.props.markdownPreview)
+        }
+    }
 
-  render () {
-    return (
-      <main>
-        <section className="markdown-container">
-          <h1 className="markdown-title">Markdown App</h1>
-          <textarea onChange={this.updatePreview} className="textarea" ref="markdown"/>
-          <div className="submit-button-container">
-          <button className="submit-button" type="submit" onClick={this.submitMarkdownNotes}>Submit</button>
-          </div>
-        </section>
-        <section className="preview-container">
-          <h1 className="preview-title">Preview</h1>
-          <div className="textarea" dangerouslySetInnerHTML={this.createMarkup()} />
-        </section>
-      </main>
-    );
-  }
+    render() {
+        return ( <main >
+            <section className = "markdown-container" >
+            <h1 className = "markdown-title" > Markdown App < /h1>
+            <textarea value={this.props.markdownNotes} onChange = { this.updatePreview } className = "textarea" ref = "markdown" / >
+            <div className = "submit-button-container" >
+            <button className = "submit-button" type = "submit" onClick = { this.submitMarkdownNotes } > Submit < /button>
+            </div>
+            </section>
+            <section className = "preview-container" >
+            <h1 className = "preview-title" > Preview < /h1>
+            <div className = "textarea"
+            dangerouslySetInnerHTML = {this.createMarkup()}/>
+            </section>
+            </main>
+        );
+    }
 }
 
 export default connect(container.allState)(AppRoot)
--- app/scripts/reducers/app_reducer.js
+++ app/scripts/reducers/app_reducer.js
@@ -1,34 +1,59 @@
+import store from '../store.js'
+
 export default function AppReducer(state, action) {
-  if (state === undefined) {
-    return {
-      markdownPreview: ''
-    };
-  }
+    if (state === undefined) {
+        return {
+            markdownPreview: '',
+            markdownNotes: ''
+        };
+    }
 
-  switch (action.type) {
-    case "UPDATE_PREVIEW":
-      return Object.assign({}, state, { markdownPreview: action.markdownPreview });
+    switch (action.type) {
+        case "UPDATE_PREVIEW":
+            return Object.assign({}, state, {
+                markdownPreview: action.markdownPreview,
+                markdownNotes: action.markdownNotes
+            });
 
-    case "SUBMIT_NOTES":
-      $.ajax({
-        type: 'POST',
-        url: 'https://api.backendless.com/v1/data/MarkdownNotes',
-        headers: {
-          "application-id": "24B65924-C870-5359-FF6E-4A5396B35700",
-          "secret-key": "BFBB0F72-782B-9CF9-FF71-D0C15271A900",
-          "application-type": "REST",
-          "Content-Type": "application/json"
-        },
-        data: JSON.stringify({
-          "Notes": action.markdownNotes
-        }),
-        success: (data, status, xhr) => {
-          console.log('something');
-        }
-      })
-      return state;
+        case "SUBMIT_NOTES":
+            let markdownNotes = action.markdownNotes;
+            console.log('markdownNotes', markdownNotes);
+            console.log('length', markdownNotes.length);
+            if (markdownNotes.length > 50) {
+                $.ajax({
+                    type: 'POST',
+                    url: 'https://api.backendless.com/v1/data/MarkdownNotes',
+                    headers: {
+                        "application-id": "24B65924-C870-5359-FF6E-4A5396B35700",
+                        "secret-key": "BFBB0F72-782B-9CF9-FF71-D0C15271A900",
+                        "application-type": "REST",
+                        "Content-Type": "application/json"
+                    },
+                    data: JSON.stringify({
+                        "Notes": action.markdownNotes
+                    }),
+                    success: (data, status, xhr) => {
+                        store.dispatch({ type: "CONFIRM_MARKDOWN_SUBMIT" });
+                        store.dispatch({ type: "EMPTY_MARKDOWN_NOTES" });
+                    }
+                })
+                return state;
+            } else {
+                return state;
+            }
 
-    default:
-      return state;
-  }
+        case "CONFIRM_MARKDOWN_SUBMIT":
+            alert('You have submitted your markdown notes to the server');
+            return state;
+
+        case "EMPTY_MARKDOWN_NOTES":
+            var newState = {
+                markdownNotes: '',
+                markdownPreview: ''
+            };
+            return Object.assign({}, state, newState);
+
+        default:
+            return state;
+    }
 }
